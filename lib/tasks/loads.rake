e9b6deb62b4f02666ca07d2b85d646d4d443e18b
Noel Kelly <noel@noelkelly.biz>
2015-05-25 23:58:22 +1000

    add lib/tasks/loads.rake

---
+++ lib/tasks/loads.rake
@@ -0,0 +1,5 @@
+namespace :loads do
+  desc "Load start table"
+  task :stars => :environment do
+  end
+end
--- spec/tasks/load_rake_spec.rb
+++ spec/tasks/load_rake_spec.rb
@@ -1,4 +1,5 @@
-RSpec.feature "Load", :type => :rake do
+#RSpec.feature "Load", :type => :rake do
   describe "load:stars" do
+    include_context "rake"
   end
-end
+#end
