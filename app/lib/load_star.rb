5b9e2aed2a57189b5b4c263144bb19a5fa331d81
Noel Kelly <noel@noelkelly.biz>
2015-05-30 23:04:01 +1000

    Add capitalize & constantize to get Command classes in app/lib/load_star.rb

--- app/lib/load_star.rb
+++ app/lib/load_star.rb
@@ -18,6 +18,7 @@ class LoadStar
     client = MediawikiApi::Client.new "http://en.wikipedia.org/w/api.php"
 
     systems.each do | system |
+      @depth = 0
       text = client.get_wikitext system
       body = text.body.force_encoding("UTF-8")
       doc = Kramdown::Document.new(body, :input => 'markdown')
@@ -26,7 +27,6 @@ class LoadStar
       puts ele.value
       if ele.type = 'root'
         @depth+=1
-        count(ele.type)
         chld ele
       end
     end
@@ -36,10 +36,13 @@ class LoadStar
 
   def chld(ele)
     #puts ele.class
+    command_class = ele.type.capitalize.to_s+'Command'
+    puts command_class
+    command = command_class.constantize
     children = ele.children
     #puts children.class
     children.each do | child |
-       #puts child.class
+      #puts child.class
       #puts '=a '+ child.to_s
       front_str = ''
       @depth.times do
@@ -48,10 +51,24 @@ class LoadStar
       puts front_str+'=b>'+child.type.to_s+'<='+child.value.to_s
       @depth+=1
       #puts 'd==='+@depth.to_s
-      count(child.type)
       chld(child)
       #puts '>8>'
       @depth-=1
     end
   end
+end
+
+class RootCommand
+end
+
+class PCommand
+end
+
+class TextCommand
+end
+
+class BlankCommand
+end
+
+class Html_elementCommand
 end
