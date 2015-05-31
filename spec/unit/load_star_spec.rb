41ee5f0bd59dace18c4cd3ce8b623d870d8c930d
Noel Kelly <noel@noelkelly.biz>
2015-05-31 23:15:29 +1000

    add spec body to spec/unit/load_star_spec.rb

--- spec/unit/load_star_spec.rb
+++ spec/unit/load_star_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 RSpec.feature "Load Star", :type => :unit do
   describe '.process' do
     it 'against...' do
+      loader = LoadStar.new
+      loader.process
+      expect().to eq ''
     end
   end
 end
