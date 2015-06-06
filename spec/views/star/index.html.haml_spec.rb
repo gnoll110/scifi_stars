a46a6941cb6bee57061eef08ee4d69e2aab5d779
Noel Kelly <noel@noelkelly.biz>
2015-06-06 23:43:27 +1000

    flesh out spec/views/star/index.html.haml_spec.rb stubs

--- spec/views/star/index.html.haml_spec.rb
+++ spec/views/star/index.html.haml_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "star/index.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:stars, [
+      Star.create!(
+        :name => "Barnard's Star",
+      ),
+      Star.create!(
+        :name => "Deneb"
+      )
+    ])
+  end
+
+  it "renders a list of companies" do
+    render
+    assert_select "p", :text => "Star Search".to_s, :count => 1
+    #assert_select "tr>td", :text => "Deneb".to_s, :count => 1
+  end
 end
