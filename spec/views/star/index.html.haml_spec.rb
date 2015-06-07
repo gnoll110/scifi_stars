034a47164e385762a91966e143df5e1d6022031f
Noel Kelly <noel@noelkelly.biz>
2015-06-07 23:50:09 +1000

    Fix spec for spec/views/star/index.html.haml_spec.rb

--- spec/views/star/index.html.haml_spec.rb
+++ spec/views/star/index.html.haml_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "star/index.html.haml", type: :view do
 
   it "renders a list of companies" do
     render
-    assert_select "p", :text => "Star Search".to_s, :count => 1
+    assert_select "body", :text => "Star#index\nFind me in app/views/star/index.html.haml", :count => 1
     #assert_select "tr>td", :text => "Deneb".to_s, :count => 1
   end
 end
